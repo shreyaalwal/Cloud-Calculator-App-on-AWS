501b314c5c09c476c0fc8ab1a2767690702775c6
shreyaalwal <shreyaalwal98@gmail.com>
2025-07-07 09:32:10 +0000

    jenkin

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.post("/calculate", (req, res) => {
   const { num1, num2, operation } = req.body;
-  let result = 0;
   const a = parseFloat(num1);
   const b = parseFloat(num2);
+  let result = 0;
 
   switch (operation) {
     case "+": result = a + b; break;
@@ -20,4 +26,7 @@ app.post("/calculate", (req, res) => {
   res.send(`<h1>Result: ${result}</h1><a href="/">Back</a>`);
 });
 
-app.listen(3000, () => console.log("Calculator running on port 3000"));
+app.listen(3000, () => {
+  console.log("Calculator App running on port 3000");
+});
+
